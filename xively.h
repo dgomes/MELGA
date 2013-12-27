52c84b79c0411074ce232e558b12fe0f81ce2b10
Diogo Gomes <diogogomes@gmail.com>
2013-12-27 22:59:52 +0000

    compiles on linux, cleanups

--- xively.h
+++ xively.h
@@ -7,11 +7,19 @@
 #include <time.h>
 #include <stdio.h>
 
+#include <syslog.h>
 #include <xi_debug.h>
 #include <xively.h>
 #include <xi_helpers.h>
 #include <xi_err.h>
 #include <mosquitto.h>
+
+#ifdef DEBUG
+#define DBG(...) \
+            do { if (DEBUG) fprintf(stderr, ##__VA_ARGS__); } while (0)
+#else
+#define DBG(fmt, ...) 
+#endif
 
 typedef struct {
 	xi_feed_t f;
@@ -26,9 +34,12 @@ typedef struct {
 	unsigned last_feed;
 } data_t;
 
-feed_t *init_feed(const char* topic, int xi_feed_id);
 void free_feed(feed_t *);
-int updateFeed(char *ApiKey, int feed_id, feed_t *feed);
+int create_feed(data_t *data, const char *topic);
+int exist_feed_topic(data_t *data, const char *topic);
 
+void message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message);
+void connect_callback(struct mosquitto *mosq, void *userdata, int result);
+void log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str);
 
 #endif //_XIVELY_H_
