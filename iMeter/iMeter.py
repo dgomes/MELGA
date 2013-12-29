60c7f00f85ab35e730e444c5cec350adcbff7a35
Diogo Gomes <dgomes@ua.pt>
2013-12-29 02:05:56 +0000

    new version, cleaner

--- iMeter/iMeter.py
+++ iMeter/iMeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import logging
 import sys
 import mosquitto
 import urllib2
@@ -8,8 +8,12 @@ from BeautifulSoup import BeautifulSoup
 
 #Customize here
 url = 'http://192.168.1.79/listdev.htm'
-feedid = "1873623042" #Xively FeedID
-apikey = "g1KD3DJBOKoLMG0hZ0jCHWgMpefJdblml9TfXWTzgMXhQHvn" #Xively API Key
+
+xively = dict(
+feedid = "1873623042", #Xively FeedID
+apikey = "g1KD3DJBOKoLMG0hZ0jCHWgMpefJdblml9TfXWTzgMXhQHvn", #Xively API Key
+datastreams = "power, energy, energySpent"
+)
 
 #DON'T EDIT BELOW!!!
 waitingFor = []
@@ -31,121 +35,75 @@ def parseWebpage():
 		power = int(data[2])
 		lastupdate = soup.findAll('td')[21].contents[0]
 	except Exception as e:
-		print e
-	return energy, power, str(lastupdate)
+		logging.exception(e)
+	return {'energy': energy,
+			'power': power,
+			'lastupdate': str(lastupdate)}
 
-def setup(mqttc):
-	#Xively
-	r, mid = mqttc.publish("imeter/xively/feedid", feedid, retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
-
-	r, mid = mqttc.publish("imeter/xively/apikey", apikey, retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
-
-	r, mid = mqttc.publish("imeter/xively/datastreams", "power, energy, energySpent", retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
-
-def on_connect(mqttc, obj, rc):
-	print "Connected"
-
-	#required for energySpent
-	r, mid = mqttc.subscribe("imeter/energy")
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
-	#we now wait for energy to be used to publish
-	waitingFor.append("pub")
-
-	if "setup" in sys.argv:
-		setup(mqttc)
-
-
-def on_subscribe(mosq, userdata, mid, granted_qos):
-	#print "on_subscribe " + str(mid) + " in " + str(waitingFor)
-
-	if mid in waitingFor:
-		waitingFor.remove(mid)
-
-def on_message(mosq, obj, msg):
-	energy, power, lastupdate = parseWebpage()
-
-	print msg.topic
-
-	#EnergySpent
-	energySpent = -1
-	if msg.topic == "imeter/energy" and len(msg.payload):
-		energySpent = int(energy) - int(msg.payload)
-		r, mid = mosq.publish("imeter/energySpent", energySpent, retain=True)
+def setup_xively(mqttc, topic, xively_conf):
+	for key in xively_conf.keys():
+		r, mid = mqttc.publish(topic + "/xively/" + key, xively_conf[key], retain=True)
 		if r != mosquitto.MOSQ_ERR_SUCCESS:
-			print "ERROR"
+			logging.error("ERROR on setup_xively")
 		else:
 			waitingFor.append(mid)
-			#we don't need energy anymore
-			mosq.unsubscribe("imeter/energy")
 
-	#Energy
-	r, mid = mosq.publish("imeter/energy", energy, retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
+def on_connect(mqttc, obj, rc):
+	logging.info("Connected")
+	if "setup" in sys.argv:
+		setup_xively(mqttc, "imeter", xively)
 
-	#LastUpdate
-	r, mid = mosq.publish("imeter/lastUpdate", lastupdate, retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
+	r, mid = mqttc.subscribe("imeter/energy")
+	waitingFor.append(mid)
 
-	#Power
-	r, mid = mosq.publish("imeter/power", power, retain=True)
-	if r != mosquitto.MOSQ_ERR_SUCCESS:
-		print "ERROR"
-	else:
-		waitingFor.append(mid)
+def on_subscribe(mqttc, userdata, mid, granted_qos):
+	logging.debug("on_subscribe " + str(mid) + " in " + str(waitingFor))
 
-	print "Last Update:" + str(lastupdate) +", Energy:" + str(energy) + ", Power:" + str(power) + ", EnergySpent:" + str(energySpent)
-
-	waitingFor.remove("pub")
-
-def on_publish(mosq, obj, mid):
-	#print "on_publish " + str(mid) + " in " + str(waitingFor)
 	if mid in waitingFor:
 		waitingFor.remove(mid)
 
-def on_log(mosq, obj, level, string):
-	print(string)
+def on_message(mqttc, userdata, msg):
+	logging.debug(msg.topic + " -> " + msg.payload)
+
+	userdata['energySpent'] = int(userdata['energy']) - int(msg.payload)
+
+def publish(mqttc, topic, data):
+	logging.info("Publish data")
+	for key in data.keys():
+		r, mid = mqttc.publish(topic + "/" + key, data[key], retain=False)
+		if r != mosquitto.MOSQ_ERR_SUCCESS:
+			logging.error("ERROR on publish")
+		else:
+			waitingFor.append(mid)
+
+def on_publish(mqttc, obj, mid):
+	logging.debug("on_publish " + str(mid) + " in " + str(waitingFor))
+	if mid in waitingFor:
+		waitingFor.remove(mid)
+
+def on_log(mqttc, obj, level, string):
+	logging.debug(string)
 
 def main():
-	print "iMeter - v0"
+	logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
+	logging.info("iMeter - v1")
 
-	mqttc = mosquitto.Mosquitto()
+	#get data
+	data = parseWebpage()
+
+	mqttc = mosquitto.Mosquitto("iMeter", True, data)
 	mqttc.on_message = on_message
 	mqttc.on_connect = on_connect
 	mqttc.on_publish = on_publish
 	mqttc.on_subscribe = on_subscribe
 	mqttc.connect("192.168.1.10", 1883, 60)
 
-
 	rc = mqttc.loop()
-	timeout = 0
 	while rc == 0 and len(waitingFor) > 0:
+		logging.debug(waitingFor)
 		rc = mqttc.loop()
-		#print waitingFor
-		#this timeout enables the detection of a deadlock in case we are publishing energy for the 1st time
-		timeout+=1
-		if timeout > 5:
-			mqttc.publish("imeter/energy")
+	rc = mqttc.loop()
+	publish(mqttc, "imeter", data)
 
 	return 0
 
