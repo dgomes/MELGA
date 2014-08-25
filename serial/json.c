56994ef7ec2ccad448e3613de5aeabbd8b67a02c
Diogo Gomes <diogogomes@gmail.com>
2014-08-26 00:41:32 +0100

    fixed leak

--- serial/json.c
+++ serial/json.c
@@ -6,24 +6,28 @@ int checkJSON_integer(const char *data, const char *name, int value)  {
 //	fprintf(stderr, "checkJSON_integer: %s\n", data);
 	json_error_t error;
 	json_t *root = json_loads(data, 0, &error);
+	json_t *code = NULL;
 	int r = -1;
 
 	if(!root) {
 		fprintf(stderr, "<%s>\n", data);
 		fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
-		return 1;
+		r=1;
+		goto exit;
 	}
 
-	json_t *code = json_object_get(root, name);
+	code = json_object_get(root, name);
 	if(!json_is_integer(code)) {
 		fprintf(stderr, "error: code is not an integer\n");
 		r=1;
+		goto exit;
 	}
 
 	if(json_integer_value(code) != value)
 		r=2;
 
-	free(root);
+	exit:
 	free(code);
+	free(root);
 	return r;
 }
