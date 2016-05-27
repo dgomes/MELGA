c6d14638ff46c7a37e78043da18ec386be1de2df
Diogo Gomes <diogogomes@gmail.com>
2016-05-28 00:15:05 +0100

    2015 edition

--- actions/xmas.py
+++ actions/xmas.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 #
-# simple script that monitors light and power to decide to turn on the lights in the kitchen
+# simple script that monitors light and power to decide to turn on the lights in the xmas
 #
+# Andy Piper @andypiper http://andypiper.co.uk
+# 
 import paho.mqtt.client as paho
 import os
 import time
@@ -11,39 +13,43 @@ import socket
 broker = "localhost"
 port = 1883
 
-def action(luminosity, power):
-#	print "luminosity = " + str(luminosity), ",", "power = " + str(power)
-	if luminosity < 750:
-		if power > 240:
-#			print "Turn ON - XMAS"
-			return "R00078043"
-		else:
-#			print "Turn OFF - XMAS"
-			return "R00078042"
-	else:
-#		print "Turn OFF - XMAS"
-		return "R00078042"
-
-def on_connect(mqttc, userdata, rc):
-	mqttc.subscribe([("imeter/power", 0), ("greenhouse/Luminosity", 0), ("weather/night", 0)])
- 
-def on_message(mqttc, userdata, message):
-#	print message.topic + " -> " + str(message.payload)	
-	userdata[message.topic] = message.payload
-
+def lights(turn=False):
+	on = "R0002B358"
+	off = "R0002B356"
 	cmd = ""
 	
-	if "weather/night" in userdata and "imeter/power" in userdata:
-		if "True" in userdata["weather/night"]:
-			cmd = action(0, int(userdata["imeter/power"]))
-		else:
-			cmd = action(1000, int(userdata["imeter/power"]))
-	
+	if turn:
+		print "Turn on - Xmas"
+		cmd = on
+	else:
+		print "Turn OFF - Xmas"
+		cmd = off
+
 	if len(cmd):
 		hostname = socket.gethostname()
 		mqttc.publish(hostname+"./ttyUSB0/cmd", cmd)
 		mqttc.publish(hostname+"./ttyUSB1/cmd", cmd)
 		mqttc.disconnect()
+
+def on_connect(mqttc, userdata, rc):
+	mqttc.subscribe([("imeter/power", 0), ("weather/night", 0), ("xmas/light", 0)])
+ 
+def on_message(mqttc, userdata, message):
+	print message.topic + " -> " + str(message.payload)	
+	userdata[message.topic] = message.payload
+
+	if "weather/night" in userdata and "imeter/power" in userdata and "xmas/light" in userdata:
+		if "True" in userdata["weather/night"]:
+			if int(userdata["imeter/power"]) > 240:
+				lights(True)		
+			elif userdata["xmas/light"] == True:
+				mqttc.publish("xmas/light", "0", retain=True)
+				mqttc.disconnect()
+			else:
+				lights(False)				
+		else:
+			lights(False)
+	
  
 mypid = os.getpid()
 client_uniq = "xmas_"+str(mypid)
